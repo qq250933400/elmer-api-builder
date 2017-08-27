5b23cf2613372861a2757e234cf5f52a2cc3c0e8
qq250933400 <250933400@qq.com>
2017-08-27 17:37:34 +0800

    fixbug

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,26 @@
 #!/usr/bin/env node
 require('colors');
 require('util');
-var initBuilder = require('./builder/init.builder');
-var helpBuilder = require('./builder/help.builder');
 var config = require('../package.json');
 var argv = process.argv;
 if (argv) {
     try {
         const command = argv[2];
-        if (command === 'init') {
-            // 初始化项目,将资源释放到项目中,创建项目结构...
-            initBuilder.init.run();
-        } else if (command === 'help') {
-            helpBuilder.helper.run(argv);
-        } else if (command === 'version' || command === 'v') {
-            console.log(config.version.green);
+        if (/node_modules/.test(__dirname)) {
+            if (command === 'init') {
+                // 初始化项目,将资源释放到项目中,创建项目结构...
+                var initBuilder = require('./builder/init.builder');
+                initBuilder.init.run();
+            } else if (command === 'help') {
+                var helpBuilder = require('./builder/help.builder');
+                helpBuilder.helper.run(argv);
+            } else if (command === 'version' || command === 'v') {
+                console.log(config.version.green);
+            } else {
+                throw new Error(`Does not support the command "${command}"`);
+            }
+        } else {
+            throw new Error('Please install this program first!');
         }
     } catch (e) {
         const msg = e.message;
