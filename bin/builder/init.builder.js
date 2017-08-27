5492a8be6dbec9f25331477b024de70946f17ef3
qq250933400 <250933400@qq.com>
2017-08-27 17:30:39 +0800

    f :

--- bin/builder/init.builder.js
+++ bin/builder/init.builder.js
@@ -31,11 +31,15 @@ var initApply = function (app) {
             const projectPath = path.resolve(__dirname, '../../../../');
             for (const fKey in files) {
                 const fileName = files[fKey];
-                const sFileName = path.resolve(sourcePath, fileName);
-                const pFileName = path.resolve(projectPath, fileName);
-                console.log(sFileName.blue);
-                console.log(pFileName.red);
+                const sFileName = path.resolve(sourcePath, fileName).replace(/\\/g, '/');
+                const pFileName = path.resolve(projectPath, fileName).replace(/\\/g, '/');
+                const pIndex = pFileName.lastIndexOf('/');
+                const pPath = pFileName.substr(0, pIndex);
+                common.checkDir(pPath);
+                const value = fs.readFileSync(sFileName, 'utf8');
+                fs.writeFileSync(pFileName, value, 'utf8');
             }
+            console.log('[0]    Project initialization success...'.green);
         }
     } else {
         throw new Error('Unable to get the allocation of resources the file content!');
